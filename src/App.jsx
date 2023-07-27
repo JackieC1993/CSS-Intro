d0b1cf67f34fea8c9a91555cf2ef80812f80a479
JackieC1993 <jackiecheung@pursuit.org>
2023-07-27 01:20:15 -0400

    added cs intro tp my app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,39 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h3>Here are some of my Skills!</h3>
+      <ul>
+        <h3>Languages</h3>
+        <li>JavaScript</li>
+        <li>SQL</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+      </ul>
+
+      <ol>
+        <h3>Libraries</h3>
+        <li>React</li>
+        <li>PostgreSQL</li>
+        <li>Node</li>
+        <li>Bootstrap</li>
+      </ol>
+    
+
+  
+      <h3>Hardest Bug So Far</h3>
+      <p>
+        My hardest bug I ever came across was this infinite loop I couldn't
+        escape.
       </p>
-    </>
+      <p>I came up with a totally sick solution though by doing ...</p>
+
+    <h2>Contact Me</h2>
+    </div>
+    
+
+       
   )
 }
 
